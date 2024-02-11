82c8bc6eb167b281cfd8ae1e1cc2cfaffbb99c0f
TrueJole <121569485+TrueJole@users.noreply.github.com>
2024-02-11 16:08:48 +0100

    Added  volumetric fog volume

--- Szenen/applySettings.gd
+++ Szenen/applySettings.gd
@@ -2,22 +2,21 @@ extends Node3D
 
 @onready var WEnvironment =  $WorldEnvironment.environment
 @onready var bakedVoxelGI = $VoxelGI
+@onready var volFog = $FogVolume
 @onready var Settings = preload('res://globalSettings.tres')
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	WEnvironment.ssao_enabled = Settings.ssao
-	WEnvironment.volumetric_fog_enabled = Settings.volumetricFog
+	volFog.visible = Settings.volumetricFog
 	WEnvironment.ssil_enabled = Settings.ssil
 	
 	WEnvironment.sdfgi_enabled = false
-	bakedVoxelGI.hide()
-	if Settings.GI == 'voxelGI':
-		bakedVoxelGI.show()
-	elif Settings.GI == 'sdfgi':
-		WEnvironment.sdfgi_enabled = true
-		print_debug('sdfgi: ',WEnvironment.sdfgi_enabled)
+	bakedVoxelGI.visible = Settings.GI == 'voxelGI'
+	
+	WEnvironment.sdfgi_enabled = Settings.GI == 'sdfgi'
+	
 		
 
 
