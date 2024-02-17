15c221b63e9b4f1bdbf7cc02196e6854e358ff59
TrueJole <121569485+TrueJole@users.noreply.github.com>
2024-02-17 23:54:28 +0100

    Hinzugefügt: Tafelflächen können gedreht werden
    + Restauration des 1*3 Wandsegmentes (UPS)

---
+++ Game Assets/KreideTafel.gd
@@ -0,0 +1,19 @@
+@tool
+extends StaticBody3D
+@onready var leftFace = $middleFace/leftFace
+@onready var rightFace = $middleFace/rightFace
+
+@export_range(0, 1) var leftRotation:float
+@export_range(0, 1) var rightRotation:float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	leftFace.rotation.y = (leftRotation)
+	rightFace.rotation.y = (rightRotation)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Engine.is_editor_hint():
+		leftFace.rotation.y = (3.14 * leftRotation)
+		rightFace.rotation.y = (-3.14 + 3.14 * (1- rightRotation))
