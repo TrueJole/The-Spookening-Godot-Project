ec7aa421ffaf538507d3b2f3a1320f27600eb17e
TrueJole <121569485+TrueJole@users.noreply.github.com>
2024-02-23 13:07:30 +0100

    Kleine Veränderungen + Sync

--- Scripts/Tür.gd
+++ Scripts/Tür.gd
@@ -10,10 +10,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	
 	if locked:
 		door.set_collision_layer_value ( 1, true )
 		door.rotation.y = 0
 		door.freeze = true
 	else:
+		#close the door if it is nearly closed
+		if snappedf(door.rotation.y, 0.1) == 0:
+			door.rotation.y = 0
+		#print_debug(door.rotation.y)
 		door.set_collision_layer_value ( 1, false )	
 		door.freeze = false
--- Szenen/Hand.gd
+++ Szenen/Hand.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends Camera3D
 
 
 
@@ -8,5 +8,16 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	if event == 'hold':
+		#FIXME: .
+		var space_state = get_world_3d().direct_space_state
+		var cam = $Camera3D
+		var mousepos = get_viewport().get_mouse_position()
+
+		var origin = cam.project_ray_origin(mousepos)
+		var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+
+		var result = space_state.intersect_ray(query)
