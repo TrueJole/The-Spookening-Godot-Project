0a3af860a9dbe993fd9b688e8b31c2ceda5fc4a9
TrueJole <121569485+TrueJole@users.noreply.github.com>
2024-03-07 16:57:41 +0100

    Feuerlöscher Hebel Animation

--- Scripts/Feuerlöscher.gd
+++ Scripts/Feuerlöscher.gd
@@ -2,8 +2,11 @@ extends RigidBody3D
 
 @onready var foamArea = $FoamArea
 @onready var foam = $GPUParticles3D
+@onready var animation = $AnimationPlayer
 
-var emit: bool = false
+
+var emit: bool = false 
+var emitting: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +15,9 @@ func _ready():
 func used():
 	print_debug('used')
 	emit = true
+	if emitting == false:
+		animation.play("feuerlöscher_an")
+	emitting = true
 	
 func _physics_process(delta):
 	if emit:
@@ -19,6 +25,9 @@ func _physics_process(delta):
 		emit = false
 		foamArea.monitorable = true
 	else:
+		if emitting:
+			animation.play_backwards("feuerlöscher_an")
+		emitting = false
 		foamArea.monitorable = false
 		foam.emitting = false
 		#foamArea.hide()
