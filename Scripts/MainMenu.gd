8c2f6f7a67c1c8a6adaae0f35a0741f7d2d0e30c
TrueJole <121569485+TrueJole@users.noreply.github.com>
2024-03-18 19:17:52 +0100

    Vorschaubild im Optionsmenu

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -24,6 +24,7 @@ func _process(_delta):
 
 
 func _on_start_button_pressed():
+	get_parent().get_node('OptionsMenu').queue_free()
 	loadingBar.show()
 	ResourceLoader.load_threaded_request(nextScene)
 	loading = true
--- Scripts/OptionsMenu.gd
+++ Scripts/OptionsMenu.gd
@@ -1,23 +1,44 @@
 extends Control
 
 var Settings: Resource
+@onready var subviewport = $PanelContainer/HBoxContainer/SubViewportContainer/SubViewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Settings = preload('res://Resources/globalSettings.tres')
-	get_node("VBoxContainer/ssaoToggleButton").button_pressed = Settings.ssao
-	get_node("VBoxContainer/volFogToggleButton").button_pressed = Settings.volumetricFog
-	get_node("VBoxContainer/SSIL").button_pressed = Settings.ssil
-	get_node("VBoxContainer/giQualitySlider").value = Settings.giQuality
-	get_node("VBoxContainer/MSAAToggleButton").button_pressed = Settings.msaa
+	Settings = load('res://Resources/globalSettings.tres')
+	get_node("PanelContainer/HBoxContainer/VBoxContainer/ssaoToggleButton").button_pressed = Settings.ssao
+	get_node("PanelContainer/HBoxContainer/VBoxContainer/volFogToggleButton").button_pressed = Settings.volumetricFog
+	get_node("PanelContainer/HBoxContainer/VBoxContainer/SSIL").button_pressed = Settings.ssil
+	get_node("PanelContainer/HBoxContainer/VBoxContainer/giQualitySlider").value = Settings.giQuality
+	get_node("PanelContainer/HBoxContainer/VBoxContainer/MSAAToggleButton").button_pressed = Settings.msaa
 	_on_gi_quality_slider_value_changed(Settings.giQuality)
 	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
+func applySettings():
+	print_debug(Settings.volumetricFog)
+	subviewport.get_node("WorldEnvironment").environment.ssao_enabled = Settings.ssao
+	subviewport.get_node("FogVolume").visible = Settings.volumetricFog
+	subviewport.get_node("WorldEnvironment").environment.ssil_enabled = Settings.ssil
+	if Settings.msaa == true:
+		RenderingServer.viewport_set_msaa_3d(get_tree().get_root().get_viewport_rid(), RenderingServer.VIEWPORT_MSAA_4X)
+	else:
+		RenderingServer.viewport_set_msaa_3d(get_tree().get_root().get_viewport_rid(), RenderingServer.VIEWPORT_MSAA_DISABLED)
+	
+	
+	
+	ProjectSettings.set_setting('rendering/global_illumination/gi/use_half_resolution', false)
+	RenderingServer.voxel_gi_set_quality(RenderingServer.VOXEL_GI_QUALITY_LOW)
+	match Settings.giQuality:
+		0:
+			subviewport.get_node("VoxelGI").visible = false
+		1:
+			subviewport.get_node("VoxelGI").visible = true
+			ProjectSettings.set_setting('rendering/global_illumination/gi/use_half_resolution', true)
+		2:
+			subviewport.get_node("VoxelGI").visible = true
+		3:
+			subviewport.get_node("VoxelGI").visible = true
+			RenderingServer.voxel_gi_set_quality(RenderingServer.VOXEL_GI_QUALITY_HIGH)
 
 func _on_back_button_pressed():
 	ResourceSaver.save(Settings)
@@ -28,10 +49,12 @@ func _on_back_button_pressed():
 
 func _on_ssao_toggle_button_toggled(toggled_on):
 	Settings.ssao = toggled_on
+	applySettings()
 
 
 func _on_vol_fog_toggle_button_toggled(toggled_on):
 	Settings.volumetricFog = toggled_on
+	applySettings()
 
 
 # @deprecated
@@ -45,26 +68,28 @@ func _on_sdfgi_toggle_button_pressed():
 	else:
 		Settings.GI = 'off'
 	print_debug(Settings.GI)
-	get_node("VBoxContainer/GIToggleButton").text = 'Global Illumination: ' + Settings.GI
-
+	get_node("PanelContainer/HBoxContainer/VBoxContainer/GIToggleButton").text = 'Global Illumination: ' + Settings.GI
+	applySettings()
 
 func _on_ssil_toggled(toggled_on):
 	Settings.ssil = toggled_on
+	applySettings()
 
 
 func _on_gi_quality_slider_value_changed(value):
 	Settings.giQuality = value
 	match Settings.giQuality:
 		0:
-			get_node("VBoxContainer/GILabel").text = 'Aus'
+			get_node("PanelContainer/HBoxContainer/VBoxContainer/GILabel").text = 'Aus'
 		1:
-			get_node("VBoxContainer/GILabel").text = 'Halbiert'
+			get_node("PanelContainer/HBoxContainer/VBoxContainer/GILabel").text = 'Halbiert'
 		2:
-			get_node("VBoxContainer/GILabel").text = 'Voll'
+			get_node("PanelContainer/HBoxContainer/VBoxContainer/GILabel").text = 'Voll'
 		3:
-			get_node("VBoxContainer/GILabel").text = 'Extra'
-
+			get_node("PanelContainer/HBoxContainer/VBoxContainer/GILabel").text = 'Extra'
+	applySettings()
 
 func _on_msaa_toggle_button_toggled(toggled_on):
 	Settings.msaa = toggled_on
+	applySettings()
 
