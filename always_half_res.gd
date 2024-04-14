e982f08973cd4498917ed8b634e76dbcabc3fa94
TrueJole <121569485+TrueJole@users.noreply.github.com>
2024-04-14 22:34:49 +0200

    immer half Res Skript

---
+++ always_half_res.gd
@@ -0,0 +1,10 @@
+@tool
+extends EditorScript
+var currentScene: Node
+
+# Called when the script is executed (using File -> Run in Script Editor).
+func _run():
+	if get_scene() != currentScene:
+		currentScene = get_scene
+		#TODO:set Half Res
+		#EditorInterface.get_editor_viewport_3d(0).
