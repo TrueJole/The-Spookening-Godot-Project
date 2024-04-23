35d6d3237327cd44b0f773125face180fbaafa3c
TrueJole <121569485+TrueJole@users.noreply.github.com>
2024-04-23 15:21:36 +0200

    Custom Scripts

---
+++ BakeAll.gd
@@ -0,0 +1,25 @@
+@tool
+extends EditorScript
+
+const VoxelGIs := ["res://Szenen/World.tscn", "res://Szenen/OptionsMenu.tscn"]
+const Occluders := ["res://Szenen/World.tscn"]
+
+# Called when the script is executed (using File -> Run in Script Editor).
+func _run():
+	var originalScene := get_scene().get_path()
+	for x in VoxelGIs:
+		EditorInterface.open_scene_from_path(x)
+		var Voxel:VoxelGI = EditorInterface.get_edited_scene_root().get_node("%VoxelGI")
+		Voxel.subdiv = VoxelGI.SUBDIV_512
+		Voxel.bake()
+		Voxel.subdiv = VoxelGI.SUBDIV_64
+		print_debug('Baked ', x)
+	
+	EditorInterface.save_all_scenes()
+	EditorInterface.open_scene_from_path(originalScene)
+	print_debug('Done!')
+	#if get_parent() is VoxelGI:
+	#	get_parent().bake()
+	#	print_debug('Baked ', get_parent())
+	#else if get_parent() is OccluderInstance3D:
+	#	get_parent().
--- always_half_res.gd
+++ always_half_res.gd
@@ -3,8 +3,8 @@ extends EditorScript
 var currentScene
 
 # Called when the script is executed (using File -> Run in Script Editor).
+
+
 func _run():
-	if get_scene() != currentScene:
-		currentScene = get_scene
-		#TODO:set Half Res
-		#EditorInterface.get_editor_viewport_3d(0).
+	EditorInterface.get_editor_viewport_3d(0).scaling_3d_scale = 0.5
+	print_debug('Applied Half Res')
