6df9f71fb6a8c099fe413e7308b349739e0d0656
TrueJole <jole.koop@gmail.com>
2024-09-29 14:00:40 +0200

    mini

--- Game Assets/Deco/candle.gd
+++ Game Assets/Deco/candle.gd
@@ -1,7 +1,10 @@
 extends StaticBody3D
 
 @export var lit: bool 
+@export var changeable: bool
 @onready var omni_light_3d: OmniLight3D = $OmniLight3D
+@onready var interactive_component: Area3D = $"Interactive Component"
+
 var justLit: int
 
 # Called when the node enters the scene tree for the first time.
@@ -13,13 +16,16 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	#print(justLit)
 	justLit -= 1
+	omni_light_3d.visible = lit
+	interactive_component.visible = changeable
 
 
 func _on_interactive_component_pressed() -> void:
-	if justLit < 0:
-		lit = not lit
-		omni_light_3d.visible = lit
-	justLit = 3
+	if changeable:
+		if justLit < 0:
+			lit = not lit
+			omni_light_3d.visible = lit
+		justLit = 3
 
 	
 	
